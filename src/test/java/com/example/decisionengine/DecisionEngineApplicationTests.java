df24d34f94beddce23d9f3d483e72f636dda4d99
Sriharivishnu Janakiraman <j.sriharivishnu@gmail.com>
2026-01-19 12:25:40 +0800

    Add src/test/java/com/example/decisionengine/DecisionEngineApplicationTests.java

---
+++ src/test/java/com/example/decisionengine/DecisionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.decisionengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
