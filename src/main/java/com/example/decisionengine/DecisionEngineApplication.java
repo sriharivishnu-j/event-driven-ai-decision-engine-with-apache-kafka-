c975e0d77dda70713c350cdda08f65f9ebb7ed11
Sriharivishnu Janakiraman <j.sriharivishnu@gmail.com>
2026-01-19 12:25:34 +0800

    Add src/main/java/com/example/decisionengine/DecisionEngineApplication.java

---
+++ src/main/java/com/example/decisionengine/DecisionEngineApplication.java
@@ -0,0 +1,11 @@
+package com.example.decisionengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DecisionEngineApplication.class, args);
+    }
+}
